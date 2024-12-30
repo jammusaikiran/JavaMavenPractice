ffdf9d90e064880d0f9aa66d8100e72ac2c9335e
saikiran <22bd1a056u@gmail.com>
2024-12-30 23:00:52 +0530

    Helo

---
+++ src/main/java/com/app/demo/PracticeJavaMaven/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.PracticeJavaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
